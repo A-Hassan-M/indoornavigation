7cb70e053d811f824b29bd1c89cf523710247fd6
Ahmed Hassan <ahmed.hassan@connectps.com>
2020-01-25 23:33:09 +0200

    init commit

---
+++ src/main/java/com/ans/indoornavigation/IndoornavigationApplication.java
@@ -0,0 +1,13 @@
+package com.ans.indoornavigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndoornavigationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndoornavigationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ans/indoornavigation/IndoornavigationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ans.indoornavigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndoornavigationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
